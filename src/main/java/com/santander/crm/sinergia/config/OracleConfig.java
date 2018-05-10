81b76892f0feb201d945953d42a99dd02d2194e1
DELGADILLO ESPARZA,MARIO RAFAEL <mario.delgadillo@gft.com>
2018-05-10 13:19:46 -0500

    Ajustes service config

--- src/main/java/com/santander/crm/sinergia/config/OracleConfig.java
+++ src/main/java/com/santander/crm/sinergia/config/OracleConfig.java
@@ -16,15 +16,14 @@ import oracle.jdbc.pool.OracleDataSource;
 @Configuration
 public class OracleConfig {
 	
-	@Value("${spring.datasource.url}")
-	//@Value("${spring.wsneo.endpoint.${spring.profiles.active}}")
-	private String urlOracle = "jdbc:oracle:thin:http://localhost:8080/ODMXNEOP";     
+	@Value("${url.oracle}")
+	private String urlOracle = "jdbc:oracle:thin:@180.181.159.40:1521:sinbande";     
 
-	@Value("${spring.datasource.username}")
-	private String userOracle = "prueba";
+	@Value("${user.oracle}")
+	private String userOracle = "sin";
 
-	@Value("${spring.datasource.password}")
-	private String passOracle = "prueba";
+	@Value("${pass.oracle}")
+	private String passOracle = "sinergia";
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(OracleConfig.class);
 	
